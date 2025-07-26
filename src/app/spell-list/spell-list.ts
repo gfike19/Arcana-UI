dbe2e55b196250c78baf7e6628225703cbccff61
gfike19 <gfike19@gmail.com>
2025-07-26 10:24:59 -0500

    added initial items, component to read in API, need to test before going further

---
+++ src/app/spell-list/spell-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-list',
+  imports: [],
+  templateUrl: './spell-list.html',
+  styles: ``
+})
+export class SpellList {
+
+}
